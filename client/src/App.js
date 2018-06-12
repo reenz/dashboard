564c7da09241d2fb1077037e2adbae70e239757e
Reena <reena.sanjay.sharma@gmail.com>
2018-06-12 12:11:27 +0100

    made a simple table by fetching data from database in toptable component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import TopTable from './components/topTable';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcome to i2C dashboard</h1>
         </header>
+        < TopTable/>
       </div>
     );
   }
---
+++ client/src/components/topTable.js
@@ -0,0 +1,54 @@
+import React, { Component} from 'react';
+import Chartkick from "chartkick";
+import ReactChartkick, { LineChart, PieChart, BarChart } from 'react-chartkick';
+import Chart from 'chart.js'
+ReactChartkick.addAdapter(Chart);
+
+class TopTable extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      topTable: []
+    }
+  }
+
+  componentDidMount() {
+    let self = this;
+    fetch('top-table')
+      .then(res => res.json())
+      .then(jsonPayload => {
+        self.setState({ topTable: jsonPayload.table })
+      });
+  }
+
+  render() {
+    let data = []
+    return (
+        <div className="Users container">
+          <h1>Users</h1>
+          <table className="table">
+          <thead>
+            <tr>
+              <th>topTable METRIC</th>
+              <th>topTable PRODUCT</th>
+              <th>topTable PCT_UPLIFT</th>
+            </tr>
+          </thead>
+          <tbody>
+              {this.state.topTable.map(row =>
+                <tr key={row.ID}>
+                  <td>{row.METRIC} </td>
+                  <td>{row.PRODUCT} </td>
+                  <td>{row.PCT_UPLIFT}</td> 
+                </tr>
+              )}
+          </tbody>
+          </table>
+        </div>
+    );
+  }
+
+}
+
+export default TopTable;
